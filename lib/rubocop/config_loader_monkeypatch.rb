e2478038067312c288329a0cccd397c2f700e440
Tim Snowhite <tsnowhite@taximagic.com>
2014-07-17 18:58:12 -0400

    [ridecharge config] config loader monkeypatch

--- lib/rubocop/config_loader_monkeypatch.rb
+++ lib/rubocop/config_loader_monkeypatch.rb
@@ -1,19 +1,57 @@
 require 'rubocop'
-module RuboCop
+module Rubocop
   class ConfigLoader
     RIDECHARGE_RUBOCOP_HOME = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
     DEFAULT_RIDECHARGE_FILE= File.join(RIDECHARGE_RUBOCOP_HOME, 'config', '0default.yml')
 
     # https://github.com/bbatsov/rubocop/blob/ff167d8f202baf7a68955db0aaf0dc29afb7e7ee/lib/rubocop/config_loader.rb#L88
 =begin 
-    def configuration_file_for(target_dir)
-      config_files_in_path(target_dir).first || DEFAULT_FILE
-    end
+      def configuration_file_for(target_dir)
+        config_files_in_path(target_dir).first || DEFAULT_FILE
+      end
+
+      def configuration_from_file(config_file)
+        config = load_file(config_file)
+        return config if config_file == DEFAULT_FILE
+
+        found_files = config_files_in_path(config_file)
+        if found_files.any? && found_files.last != config_file
+          print 'AllCops/Excludes ' if debug?
+          add_excludes_from_higher_level(config, load_file(found_files.last))
+        end
+        make_excludes_absolute(config)
+        merge_with_default(config, config_file)
+      end
+      def default_configuration
+        @default_configuration ||= begin
+                                     print 'Default ' if debug?
+                                     load_file(DEFAULT_FILE)
+                                   end
+      end
 =end
     class << self
       def configuration_file_for(target_dir)
-        $stderr.puts DEFAULT_RIDECHARGE_FILE
+        $stderr.puts [target_dir, DEFAULT_RIDECHARGE_FILE].inspect
         config_files_in_path(target_dir).first || DEFAULT_RIDECHARGE_FILE
+      end
+      def configuration_from_file(config_file)
+        $stderr.puts [config_file, DEFAULT_RIDECHARGE_FILE].inspect
+        config = load_file(config_file)
+        return config if config_file == DEFAULT_RIDECHARGE_FILE
+
+        found_files = config_files_in_path(config_file)
+        if found_files.any? && found_files.last != config_file
+          print 'AllCops/Excludes ' if debug?
+          add_excludes_from_higher_level(config, load_file(found_files.last))
+        end
+        make_excludes_absolute(config)
+        merge_with_default(config, config_file)
+      end
+      def default_configuration
+        @default_configuration ||= begin
+                                     print 'Ridecharge Default ' if debug?
+                                     load_file(DEFAULT_RIDECHARGE_FILE)
+                                   end
       end
     end
   end
